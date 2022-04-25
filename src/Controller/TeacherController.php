9f367403e947aef71ce3f44b1cbc436b737f0e93
minhtay150601@ <90816499+taydmgch190016@users.noreply.github.com>
2022-04-25 13:32:34 +0700

    Resolving conflicts
    teachercontroller

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -9,8 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-
 #[Route('/teacher')]
 class TeacherController extends AbstractController
 {
